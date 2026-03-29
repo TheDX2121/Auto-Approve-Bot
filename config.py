d3627904db3604971b33ff129e018428d865c891
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2026-03-29 22:42:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN = int(os.environ.get("ADMIN", "5094080507"))
 PICS = (os.environ.get("PICS", "https://i.ibb.co/MDssddJp/pic.jpg https://i.ibb.co/n8fQ2xcx/pic.jpg")).split()
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002686843200"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003615234781"))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "True").lower() == "true"
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "approve")
